0ee6e89d8370eb129c4e637c2fa4245f1d7cf770
FarinaMatteo <matteo.farina-1@studenti.unitn.it>
2021-05-19 18:00:32 +0200

    sampling by img index to provide batch size consistency across iterations, average loss option

--- functions.py
+++ functions.py
@@ -5,51 +5,127 @@ import torch
 from torchvision import transforms
 import torchvision
 import pandas as pd
-import os,glob
+import os
+import glob
+import shutil
 from torchvision.io import read_image
 
+def flatten_folder(folder, target=None):
+    # append a trailing slash to the folder names if needed
+    folder = folder if folder.endswith("/") else folder+"/"
+    if not target: target = folder
+    else: target = target if target.endswith("/") else target+"/"
+    # retrieve the subtree of the current folder and start parsing it
+    file_list = glob.glob(folder + "*")
+    for f in file_list:
+        # base case, directly copy the src file to the target location
+        if os.path.isfile(f):
+            shutil.move(src=f, dst=target+f"{os.path.basename(f)}")
+        # more complicated scenario, start recursion keeping the same target
+        elif os.path.isdir(f):
+            flatten_folder(f, target)
+            os.rmdir(f)
+
+def group_folder(folder):
+    folder = folder if folder.endswith("/") else folder+"/"
+    file_list = glob.glob(folder + "*")
+    ids = []
+    for f in file_list:
+        assert not os.path.isdir(f)
+        cur_id = os.path.basename(f)[:4]
+        # trim starting zeros
+        while cur_id.startswith("0"):
+            cur_id = cur_id[1:]
+        # create new id directory if needed
+        if cur_id not in ids:
+            os.makedirs(os.path.join(folder, cur_id))
+        
+        ids.append(cur_id)
+        # # move the current file in its directory group
+        shutil.move(src=f, dst=os.path.join(folder, cur_id, os.path.basename(f)))
+
+    
+
 class CustomImageDataset(torch.utils.data.Dataset):
-    def __init__(self, annotations_file, img_dir, transform, target_transform=None):
+    def __init__(self, annotations_file, img_dir, transform, target_transform=None, batch_mode="id"):
         super(CustomImageDataset,self).__init__()
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
+        self.batch_mode = batch_mode
+        if self.batch_mode == "img":
+            self.img_list = os.listdir(self.img_dir)
         
     def __len__(self):
-        return len(self.img_labels)
+        if self.batch_mode == "id":
+            return len(self.img_labels)
+        elif self.batch_mode == "img":
+            return len(self.img_list)
 
     def __getitem__(self, idx):
-        folder_path = str(str(self.img_dir) +"/" + str(self.img_labels.iloc[idx, 0]))
-        image = []
-        for filename in glob.glob(os.path.join(folder_path, '*.jpg')):
-            with open(filename, 'r') as f:    
-                tmp_image = read_image(filename)
-                image.append(tmp_image/255)
-                # break ##--> TO RETURN ONLY ONE IMAGE AND AVOID THE BATCH SIZE PROBLEM IN THE DATALOADER
-
-        label = []
-        for i in range(1,28):
-            label.append(self.img_labels.iloc[idx, i]-1)
-        
-        if self.transform:
-            image_tmp = image
-            del image
+        # getitem based on :param idx: being someone's id. Thus, it should point to a folder containing
+        # possibly more than one image.
+        if self.batch_mode == "id":
+            folder_path = str(str(self.img_dir) +"/" + str(self.img_labels.iloc[idx, 0]))
             image = []
-            for item in image_tmp:
-                new = self.transform(item) # transforms.ToPILImage()(item).convert("RGB")
-                image.append(new)
+            for filename in glob.glob(os.path.join(folder_path, '*.jpg')):
+                with open(filename, 'r') as f:    
+                    tmp_image = read_image(filename)
+                    image.append(tmp_image/255)
+                    # break ##--> TO RETURN ONLY ONE IMAGE AND AVOID THE BATCH SIZE PROBLEM IN THE DATALOADER
+
+            # load annotation vector, excluding the id column
+            label = []
+            for i in range(1,len(self.img_labels.iloc[idx])):
+                label.append(self.img_labels.iloc[idx, i]-1)
+            
+            if self.transform:
+                image_tmp = image
+                del image
+                image = []
+                for item in image_tmp:
+                    new = self.transform(item) # transforms.ToPILImage()(item).convert("RGB")
+                    image.append(new)
+            
+            if self.target_transform:
+                label = self.target_transform(label)
         
-        if self.target_transform:
-            label = self.target_transform(label)
-    
-        sample = {"label": label, "image" : image}
-        return sample
+            sample = {"label": label, "image" : image}
+            return sample
+        
+        # otherwise, retrieve data by indexing images inside the provided folder
+        else:
+            # load the tensor image using cached files
+            img_path = self.img_list[idx]
+            img = read_image(os.path.join(self.img_dir, img_path))/255.0
+            
+            # load the annotations vector, excluding the id column
+            person_id = os.path.basename(img_path)[:4]
+            
+            # shrink person id
+            while person_id.startswith("0"): person_id = person_id[1:]
+            person_id = int(person_id)
+            
+            # load the label based on the person id
+            label = self.img_labels.loc[(self.img_labels["id"]==person_id)]
+            label = label.to_numpy()
+            label = torch.from_numpy(label).squeeze()[1:]
+            
+            if self.transform:
+                img = self.transform(img)
+            
+            if self.target_transform:
+                label = self.target_transform(label)
+            
+            return img, label
+
+
 
 
 # END Functions -------------------------------------------------------------
 
-def get_dataset(folder_images, folder_label, transformations):
+def get_dataset(folder_images, folder_label, transformations, batch_mode):
 
     # initialize list of transformations to apply
     transforms_list = []
@@ -78,7 +154,7 @@ def get_dataset(folder_images, folder_label, transformations):
     # missing
    
     # generate the dataset and return it 
-    return CustomImageDataset(folder_label, folder_images, transform=transforms.Compose(transforms_list))
+    return CustomImageDataset(folder_label, folder_images, transform=transforms.Compose(transforms_list), batch_mode=batch_mode)
 
 
 def print_images(set, transformation , train):
--- train.py
+++ train.py
@@ -3,104 +3,102 @@ import torch
 import torchvision
 
 def initialize_alexnet(num_classes):
-  # load the pre-trained Alexnet
-  alexnet = torchvision.models.alexnet(pretrained=True)
-  
-  # get the number of neurons in the penultimate layer
-  in_features = alexnet.classifier[6].in_features
-  
-  # re-initalize the output layer
-  alexnet.classifier[6] = torch.nn.Linear(in_features=in_features, 
-                                          out_features=num_classes)
-  
-  return alexnet
+    # load the pre-trained Alexnet
+    alexnet = torchvision.models.alexnet(pretrained=True)
+
+    # get the number of neurons in the penultimate layer
+    in_features = alexnet.classifier[6].in_features
+
+    # re-initalize the output layer
+    alexnet.classifier[6] = torch.nn.Linear(in_features=in_features,
+                                        out_features=num_classes)
+
+    return alexnet
 
 def initialize_resnet50(num_classes):
-  # load the pre-trained resnet50
-  resnet50 = torchvision.models.resnet50(pretrained=True)
-  in_features = resnet50.fc.in_features
-  resnet50.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
-  return resnet50
+    # load the pre-trained resnet50
+    resnet50 = torchvision.models.resnet50(pretrained=True)
+    in_features = resnet50.fc.in_features
+    resnet50.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
+    return resnet50
 
 def initialize_resnet18(num_classes):
-  # load the pre-trained resnet18
-  resnet18 = torchvision.models.resnet18(pretrained=True)
-  in_features = resnet18.fc.in_features
-  resnet18.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
-  return resnet18
+    # load the pre-trained resnet18
+    resnet18 = torchvision.models.resnet18(pretrained=True)
+    in_features = resnet18.fc.in_features
+    resnet18.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
+    return resnet18
 
 def initialize_resnet34(num_classes):
-  # load the pre-trained resnet50
-  resnet34 = torchvision.models.resnet34(pretrained=True)
-  in_features = resnet34.fc.in_features
-  resnet34.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
-  return resnet34
+    # load the pre-trained resnet50
+    resnet34 = torchvision.models.resnet34(pretrained=True)
+    in_features = resnet34.fc.in_features
+    resnet34.fc = torch.nn.Linear(in_features=in_features, out_features=num_classes)
+    return resnet34
 
 def get_optimizer(model, lr, wd, momentum):
-  
-  # we will create two groups of weights, one for the newly initialized layer
-  # and the other for rest of the layers of the network
-  
-  final_layer_weights = []
-  rest_of_the_net_weights = []
-  
-  # we will iterate through the layers of the network
-  for name, param in model.named_parameters():
-    if name.startswith('classifier.6') or name.startswith("fc"):
-      final_layer_weights.append(param)
-    else:
-      rest_of_the_net_weights.append(param)
+    # we will create two groups of weights, one for the newly initialized layer
+    # and the other for rest of the layers of the network
+    final_layer_weights = []
+    rest_of_the_net_weights = []
 
-  # so now we have divided the network weights into two groups.
-  # We will train the final_layer_weights with learning_rate = lr
-  # and rest_of_the_net_weights with learning_rate = lr / 10
-  
-  optimizer = torch.optim.SGD([
-      {'params': rest_of_the_net_weights},
-      {'params': final_layer_weights, 'lr': lr}
-  ], lr=lr / 10, weight_decay=wd, momentum=momentum)
-  
-  return optimizer
+    # we will iterate through the layers of the network
+    for name, param in model.named_parameters():
+        if name.startswith('classifier.6') or name.startswith("fc"):
+            final_layer_weights.append(param)
+        else:
+            rest_of_the_net_weights.append(param)
+
+    # so now we have divided the network weights into two groups.
+    # We will train the final_layer_weights with learning_rate = lr
+    # and rest_of_the_net_weights with learning_rate = lr / 10
+    optimizer = torch.optim.SGD([
+        {'params': rest_of_the_net_weights},
+        {'params': final_layer_weights, 'lr': lr}
+    ], lr=lr / 10, weight_decay=wd, momentum=momentum)
+
+    return optimizer
 
 
-def train(net, loader, optimizer, device='cuda:0'):
+def train(net, loader, optimizer, avg_loss=True, device='cuda:0'):
     num_samples = 0.0
     total_loss = 0.0
     total_up_acc = 0.
     total_down_acc = 0.
     total_age_acc = 0.
     total_rest_acc = 0.
-    
+
     # define loss functions to be used throughout the process
     ce_loss = torch.nn.CrossEntropyLoss()
     bce_loss = torch.nn.BCELoss()
 
     # select the device according to system hardware
     if device.startswith("cuda") and torch.cuda.is_available():
-      device = 'cuda:0'
+        device = 'cuda:0'
     else:
-      device = 'cpu'
-    
+        device = 'cpu'
+
     # put the network in training mode
     net.train()
-    
+
     for (images, labels) in loader:
+                
         # move tensors onto gpu if needed
         if device.startswith("cuda"):
-          images = images.to(device)
-          labels = labels.to(device)
+            images = images.to(device)
+            labels = labels.to(device)
         else:
-          images = images.cpu()
-          labels = labels.cpu()
-        
+            images = images.cpu()
+            labels = labels.cpu()
+
         # apply a forward pass
         preds = net(images)
-        
+
         # compute the cross entropy loss for the first 4 predictions (age-related)
         age_preds = preds[:, :4]
         age_labels = labels[:, 0]
         age_loss = ce_loss(age_preds, age_labels)
-        
+
         # compute losses for the first 9 independent features
         independent_labels = labels[:, 1:10].float()
         independent_preds = preds[:, 4:13]
@@ -109,7 +107,7 @@ def train(net, loader, optimizer, device='cuda:0'):
         ind_loss = bce_loss(independent_preds, independent_labels)
 
         # compute losses for upper and lower body clothing color
-        ## upper body cross entropy loss 
+        ## upper body cross entropy loss
         up_labels = torch.argmax(labels[:, 10:19], dim=1)
         up_preds = preds[:, 13:22]
         up_ce_loss = ce_loss(up_preds, up_labels)
@@ -121,15 +119,18 @@ def train(net, loader, optimizer, device='cuda:0'):
 
         # compute overall loss
         loss = up_ce_loss + down_ce_loss + ind_loss + age_loss
-        
+        # normalize loss based on settings
+        if avg_loss:
+            loss /= 4
+
         # backprop
         loss.backward()
-        
+
         # optimize and reset
         optimizer.step()
-        
+
         optimizer.zero_grad()
-        
+
         # update stats
         num_samples += images.shape[0]
         total_loss += loss.item()
@@ -140,41 +141,43 @@ def train(net, loader, optimizer, device='cuda:0'):
         # update stats for bc accuracy
         total_rest_acc += independent_preds.round().eq(independent_labels).sum().item()
 
-    return total_loss/num_samples, total_up_acc/num_samples*100, total_down_acc/num_samples*100, total_age_acc/num_samples*100, (total_rest_acc/(num_samples*9))*100
+    return total_loss/num_samples, total_up_acc/num_samples*100, total_down_acc/num_samples*100,\
+        total_age_acc/num_samples*100, (total_rest_acc/(num_samples*9))*100
 
 
-def test(net, loader, device="cuda:0"):
+def test(net, loader, avg_loss=True, device="cuda:0"):
     num_samples = 0.0
     total_loss = 0.0
     total_up_acc = 0.
     total_down_acc = 0.
     total_age_acc = 0.
     total_rest_acc = 0.
-    
+
     # define loss functions to be used throughout the process
     ce_loss = torch.nn.CrossEntropyLoss()
     bce_loss = torch.nn.BCELoss()
 
     # select the device according to system hardware
     if device.startswith("cuda") and torch.cuda.is_available():
-      device = 'cuda:0'
+        device = 'cuda:0'
     else:
-      device = 'cpu'
-    
+        device = 'cpu'
+
     # put the network in evaluation mode
     net.eval()
-    
+
     # set context manager to avoid gradients computation
     with torch.no_grad():
-        for i, (images, labels) in enumerate(loader):
+        for (images, labels) in enumerate(loader):
+
             # move tensors onto gpu if needed
             if device.startswith("cuda"):
-              images = images.to(device)
-              labels = labels.to(device)
+                images = images.to(device)
+                labels = labels.to(device)
             else:
-              images = images.cpu()
-              labels = labels.cpu()
-            
+                images = images.cpu()
+                labels = labels.cpu()
+
             # apply a forward pass
             preds = net(images)
 
@@ -182,7 +185,7 @@ def test(net, loader, device="cuda:0"):
             age_preds = preds[:, :4]
             age_labels = labels[:, 0]
             age_loss = ce_loss(age_preds, age_labels)
-            
+
             # compute losses for the first 9 independent features
             independent_labels = labels[:, 1:10].float()
             independent_preds = preds[:, 4:13]
@@ -191,7 +194,7 @@ def test(net, loader, device="cuda:0"):
             ind_loss = bce_loss(independent_preds, independent_labels)
 
             # compute losses for upper and lower body clothing color
-            ## upper body cross entropy loss 
+            ## upper body cross entropy loss
             up_labels = torch.argmax(labels[:, 10:19], dim=1)
             up_preds = preds[:, 13:22]
             up_ce_loss = ce_loss(up_preds, up_labels)
@@ -203,7 +206,10 @@ def test(net, loader, device="cuda:0"):
 
             # compute overall loss
             loss = up_ce_loss + down_ce_loss + ind_loss + age_loss
-            
+            # normalize loss based on settings
+            if avg_loss:
+                loss /= 4
+
             # update stats for loss
             num_samples += images.shape[0]
             total_loss += loss.item()
@@ -214,15 +220,16 @@ def test(net, loader, device="cuda:0"):
             # update stats for bc accuracy
             total_rest_acc += independent_preds.round().eq(independent_labels).sum().item()
 
-    return total_loss/num_samples, total_up_acc/num_samples*100, total_down_acc/num_samples*100, total_age_acc/num_samples*100, (total_rest_acc/(num_samples*9))*100
+    return total_loss/num_samples, total_up_acc/num_samples*100, total_down_acc/num_samples*100,\
+        total_age_acc/num_samples*100, (total_rest_acc/(num_samples*9))*100
 
 
-def classification_train(net, tr_loader, val_loader, optimizer, writer, epochs=30, save_path="networks/baseline.pth", patience=5):
-    
-    training_loss, tr_up_acc, tr_down_acc, tr_age_acc, tr_rest_acc = test(net, tr_loader)
-    validation_loss, val_up_acc, val_down_acc, val_age_acc, val_rest_acc = test(net, val_loader)
-    
-    print("=" * 50, "\n")
+def classification_train(net, tr_loader, val_loader, optimizer, writer, avg_loss=True, epochs=30, save_path="networks/baseline.pth", patience=5):
+
+    training_loss, tr_up_acc, tr_down_acc, tr_age_acc, tr_rest_acc = test(net, tr_loader, avg_loss)
+    validation_loss, val_up_acc, val_down_acc, val_age_acc, val_rest_acc = test(net, val_loader, avg_loss)
+
+    print("=" * 100, "\n")
     print("Values Before Training:")
     print("\tTraining Loss: {:.4f}".format(training_loss))
     print("\tTrain Up Accuracy: {:.2f}".format(tr_up_acc))
@@ -244,14 +251,14 @@ def classification_train(net, tr_loader, val_loader, optimizer, writer, epochs=3
     writer.add_scalar("Accuracy/validation/lowerbody_accuracy", val_down_acc, 0)
     writer.add_scalar("Accuracy/validation/age_accuracy", val_age_acc, 0)
     writer.add_scalar("Accuracy/validation/binary_accuracy_avg", val_rest_acc, 0)
-    
+
     es_epochs = 0
     best_loss = 1_000_000_000_000
     for e in range(epochs):
-        training_loss, tr_up_acc, tr_down_acc, tr_age_acc, tr_rest_acc = train(net, tr_loader, optimizer)
-        validation_loss, val_up_acc, val_down_acc, val_age_acc, val_rest_acc = test(net, val_loader)
+        training_loss, tr_up_acc, tr_down_acc, tr_age_acc, tr_rest_acc = train(net, tr_loader, optimizer, avg_loss)
+        validation_loss, val_up_acc, val_down_acc, val_age_acc, val_rest_acc = test(net, val_loader, avg_loss)
 
-        print("=" * 50, "\n")
+        print("=" * 100, "\n")
         print("Values After Training Epoch {}".format(e+1))
         print("\tTraining Loss: {:.4f}".format(training_loss))
         print("\tTrain Up Accuracy: {:.2f}".format(tr_up_acc))
@@ -281,14 +288,17 @@ def classification_train(net, tr_loader, val_loader, optimizer, writer, epochs=3
         else:
             es_epochs = 0
             best_loss = validation_loss
+            # save the best model according to the provided path
+            if not os.path.exists(os.path.dirname(save_path)): 
+                os.makedirs(os.path.dirname(save_path))
+            print("\tModel Loss improved, saving at checkpoint at: {}".format(save_path))
+            torch.save(net.state_dict(), save_path)
+
         # check against the patience and exit due to early stopping conditions
         if es_epochs > patience:
-          print("Early Stopping has been triggered. Exiting the training pipeline.")
-          break
+            print("\nEarly Stopping has been triggered. Exiting the training pipeline.\n")
+            break
 
-    if not os.path.exists(os.path.dirname(save_path)):
-        os.makedirs(os.path.dirname(save_path))
-    torch.save(net.state_dict(), save_path)
     return net
 
 
