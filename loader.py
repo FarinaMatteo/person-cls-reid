d9db53fae7ac73387694ecb4eb550e41a77b51a2
FarinaMatteo <matteo.farina-1@studenti.unitn.it>
2021-05-19 18:00:32 +0200

    sampling by img index to provide batch size consistency across iterations, average loss option

--- loader.py
+++ loader.py
@@ -1,12 +1,18 @@
 import os
 import torch
-from torch.utils.tensorboard import SummaryWriter
 from split import split
 from functions import *
+import multiprocessing as mp
+from torch.utils.tensorboard import SummaryWriter
 from train import classification_train, initialize_alexnet, initialize_resnet50, initialize_resnet18, initialize_resnet34, get_optimizer
 
 
-def main(network="alexnet", batch_size=16, device="cuda:0", exp_name="baseline"):
+def main(network="alexnet", batch_size=16,
+        batch_mode="id", max_images=None,
+        train_split=0.8, avg_loss=True,
+        device="cuda:0", exp_name="baseline"):
+    assert batch_mode in ("id", "img")
+
     # defining folders with data and files with annotations
     folder_labels_train = "csv_files/train_label.csv"
     folder_images_train = "train_directory"
@@ -15,25 +21,35 @@ def main(network="alexnet", batch_size=16, device="cuda:0", exp_name="baseline")
     folder_images_val= "validation_directory"
     
     # creating the Train and Validation sets if needed
-    print("=" * 50)
+    print("=" * 100)
     if not os.path.exists(folder_images_train) or len(os.listdir(folder_images_train))==0:
-        print("\nSplitting the whole dataset into Train and Validation sets.\n")
-        split()
+        print("\nSplitting the whole dataset into Train and Validation sets.")
+        print("Your current configuration is:\n\tTrain split: {}\n\tMax Images:{}\n".format(train_split, max_images))
+        split(train_split=train_split, max_images=max_images)
     else:
         print("\nTrain and Validation sets already exist. No need to recreate them.\n")
     
+    # access elements by idx in the fs
+    if batch_mode=="img":
+        flatten_folder(folder_images_train)
+        flatten_folder(folder_images_val)
+    
     # creating the datasets based on the output of the previous split(s)
     transform_list = ["flip", "erasing", "rotation", "color_jitter"]
-    train = get_dataset(folder_images_train, folder_labels_train, transform_list)
-    val = get_dataset(folder_images_val, folder_label_val, transform_list)
+    train = get_dataset(folder_images_train, folder_labels_train, transform_list, batch_mode)
+    val = get_dataset(folder_images_val, folder_label_val, transform_list, batch_mode)
 
-    # feedinf the respective dataloaders with the datasets    
-    print("=" * 50)
+    # feeding the respective dataloaders with the datasets    
+    print("=" * 100)
     print("\nInitializing Train and Validation DataLoaders...\n")
-    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
-    val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-   
-    print("=" * 50)
+    if batch_mode == "id":
+        train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, num_workers=mp.cpu_count()//2)
+        val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, num_workers=mp.cpu_count()//2)
+    elif batch_mode == "img":
+        train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=mp.cpu_count()//2)
+        val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=mp.cpu_count()//2)
+
+    print("=" * 100)
     print("\nInitializing the network and starting the training pipeline...\n")
     # define the writer to monitor data with Tensorboard
     writer = SummaryWriter(log_dir="experiments/{}".format(exp_name))
@@ -55,13 +71,13 @@ def main(network="alexnet", batch_size=16, device="cuda:0", exp_name="baseline")
     optimizer = get_optimizer(net, lr=0.001, wd=1e-4, momentum=0.0009)
 
     # start the training pipeline
-    classification_train(net, train_loader, val_loader, optimizer, writer, save_path=f"networks/{exp_name}/model.pth")
+    classification_train(net, train_loader, val_loader, optimizer, avg_loss, writer, save_path=f"networks/{exp_name}/model.pth")
         
-    print("=" * 50)
-    print("\nTraining finished. Launch 'tensorboard --logdir=experiments' to monitor the learning curves.")
+    print("=" * 100)
+    print("\nTraining finished. Launch 'tensorboard --logdir=experiments' to monitor the learning curves.\n")
     return
         
 if __name__ == "__main__":
-    main(network="resnet18", exp_name="resnet18_multiloss")
-    main(network="resnet34", exp_name="resnet34_multiloss")
-    main(network="resnet50", exp_name="resnet50_multiloss")
+    main(network="resnet18", batch_mode="img", exp_name="resnet18_avgmultiloss")
+    main(network="resnet34", batch_mode="img", exp_name="resnet34_avgmultiloss")
+    main(network="resnet50", batch_mode="img", exp_name="resnet50_avgmultiloss")
--- split.py
+++ split.py
@@ -6,20 +6,19 @@ The choice of which ID goes to one set or another is completely randomic due to 
 
 '''
 
-import os,glob
+import os
+import glob
 import random
 import shutil
 import re
 import pandas as pd
 from multicolor import *
 
-#DEFINE GLOBAL VARIABLES
-def split():
+# DEFINE GLOBAL VARIABLES
+def split(train_split=0.8, max_images=None):
     folder_path = 'dataset/train'
-    train = 0.8
-    val = 0.2
 
-    #DELETE FOLDERS IF AVAILABLE
+    # DELETE FOLDERS IF AVAILABLE
     try:
         shutil.rmtree('train_directory/')
     except FileNotFoundError as exc:
@@ -30,87 +29,83 @@ def split():
     except FileNotFoundError as exc:
         print("No validation directory found on your system, creating one.")
 
-    #INITIALIZE NEW FOLDERS (for validation and train set)
+    # INITIALIZE NEW FOLDERS (for validation and train set)
     try:
         os.mkdir('train_directory/')
     except FileExistsError as exc:
         print(exc)
-
 
     try:
         os.mkdir('validation_directory/')
     except FileExistsError as exc:
         print(exc)
 
-
-    #useless --> to remove 
+    # useless --> to remove 
     i=0
 
     random_list = []
     df = pd.DataFrame(columns=['id','name'])
 
-    ##for each image inside the specified folder save the respective ID and NAME of the image eg: 0009_c4_0963839.jpg
+    # # for each image inside the specified folder save the respective ID and NAME of the image eg: 0009_c4_0963839.jpg
     for filename in glob.glob(os.path.join(folder_path, '*.jpg')):
         with open(filename, 'r') as f:
             row = {}
 
-            #keeping only the image name without the path
+            # keeping only the image name without the path
             filename = str(filename.replace("dataset/train/",""))
             row["name"]= filename
 
-            #Keeping only the ID
+            # Keeping only the ID
             filename = re.sub(r'_.*',"",filename)
 
-            #Add the id to the ID's list
+            # Add the id to the ID's list
             random_list.append(filename)
             
             row["id"] = filename
             
-            #save each couple ID- NAME(image) into a DataFrame
+            # save each couple ID- NAME(image) into a DataFrame
             df = df.append(row, ignore_index=True)
         
-            #useless --> to remove  
-            # print(i)
-            # i += 1
-            # if i == 10:
-            #      break
+        i += 1
+        if max_images and i==max_images:
+            print("Reached the {} limit, no further images will be placed in the dataset folder".format(max_images))
+            break
 
-    #Group each image based on the ID value
+    # Group each image based on the ID value
     df = df.groupby('id')['name'].apply(list).reset_index()
 
-    #save the csv with the link between ID and Name
+    # save the csv with the link between ID and Name
     df.to_csv('csv_files/file.csv', index=False)
 
-    #Remove duplicate ID and shuffle the ID's list
+    # Remove duplicate ID and shuffle the ID's list
     random_list = list(set(random_list))
     random.shuffle(random_list)
 
-    #Counter to be sure we use ALL the images inside the Folder selected
+    # Counter to be sure we use ALL the images inside the Folder selected
     counter = 0
 
-
-    #I select the test% of ID to put inside the train folder
-    #ATTENTION!!! --> We are NOT splitting test% val% our images ---> we select test% of IDS INDEPENDETLY of how many images are related to each ID
+    # I select the test% of ID to put inside the train folder
+    # ATTENTION!!! --> We are NOT splitting test% val% our images ---> we select test% of IDS INDEPENDETLY of how many images are related to each ID
 
     df1 = pd.DataFrame(columns=['id'])
 
-    for i in range (0,int(len(random_list)*train)):
+    for i in range (0,int(len(random_list)*train_split)):
         
-        #Looking for the row of the DataFrame which contains the selected ID
+        # Looking for the row of the DataFrame which contains the selected ID
         l = df[df['id'] == random_list[i]].to_dict('records')[0]
         
-        ##Add to a list of ID selected for the train set
+        # # Add to a list of ID selected for the train set
         row = {}
         row['id']= int(random_list[i])
         df1 = df1.append(row, ignore_index=True)    
 
-        #Create a new folder inside train_directory in order to put all the images related to the ID
+        # Create a new folder inside train_directory in order to put all the images related to the ID
         try:
             os.mkdir('train_directory/'+ str(int(random_list[i])))
         except FileExistsError as exc:
             print(exc)
 
-        ##Adding ALL ID's images to its respective folder
+        # # Adding ALL ID's images to its respective folder
         for key,value in l.items():
             if (key=='name'):
                 l2 = list(value)
@@ -124,17 +119,17 @@ def split():
 
 
 
-    ##create and save a new annotations.csv which is the result of the intersection between the Train IDs and the original annotations file
-    #df1.to_csv('csv_files/train.csv', index=False)
-    #df2 = pd.read_csv('csv_files/train.csv')
+    # create and save a new annotations.csv which is the result of the intersection between the Train IDs and the original annotations file
+    # df1.to_csv('csv_files/train.csv', index=False)
+    # df2 = pd.read_csv('csv_files/train.csv')
 
     df2 = pd.read_csv('dataset/annotations_train.csv')
-    int_df = pd.merge(df2, df1, how ='inner', on =['id'])
+    int_df = pd.merge(df2, df1, how='inner', on=['id'])
     int_df = multi_color(int_df)
     int_df.to_csv('csv_files/train_label.csv',index=False)
 
 
-    ##THE SAME AS FOR THE TRAIN IS REPETED ALSO FOR THE VALIDATION
+    # THE SAME IS REPEATED ALSO FOR THE VALIDATION
     df1 = pd.DataFrame()
     for i in range(counter,len(random_list)):
         l = df[df['id'] == random_list[i]].to_dict('records')[0]
